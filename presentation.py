bcd3f02288f1a650cb63e590d5be1f99ad3e7904
Ildar <i-shamsutdinov@inbox.ru>
2025-05-21 22:04:11 +0300

    Презентация итоги

--- presentation.py
+++ presentation.py
@@ -9,7 +9,9 @@ def presentation_page():
     # Прогнозирование отказов оборудования
     ---
     ## Введение
-    - Описание задачи и датасета.
+    - Задача: сразработать модель машинного обучения, которая предсказывает, произойдет ли отказ оборудования или нет.
+    - Используемой датасет: Используется датасет "AI4I 2020 Predictive Maintenance Dataset", содержащий 10 000 записей с 14 признаками.
+    - Ссылка на датасет: https://archive.ics.uci.edu/dataset/601/predictive+maintenance+data
     - Цель: предсказать отказ оборудования (Target = 1) или его отсутствие
     (Target = 0).
     ---
@@ -20,12 +22,47 @@ def presentation_page():
     4. Оценка модели.
     5. Визуализация результатов.
     ---
+    ### 1. Загрузка данных.
+    Для загрузки использовалась библиотека ucimlrepo
+    ai4i_2020_predictive_maintenance_dataset = fetch_ucirepo(id=601)
+    ---
+    ### 2. Предобработка данных.
+    Для обучения необходимо предобработать данные, убрав лишние столбцы, которые не будут учавствовать в обучении
+    data = data.drop(columns=['UDI', 'Product ID', 'TWF', 'HDF', 'PWF', 'OSF', 'RNF'])
+    Также надо разделить данные на тренировочные и тестовые множества
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    --- 
+    ### 3. Обучение модели.
+    После предобработки запускаем обучение модели
+    model = LogisticRegression()
+    model.fit(X_train, y_train)
+    ---
+    ### 4. Оценка модели.
+    Оценка модели на тестовых данных
+    y_pred = model.predict(X_test)
+    accuracy = accuracy_score(y_test, y_pred)
+    conf_matrix = confusion_matrix(y_test, y_pred)
+    classification_rep = classification_report(y_test, y_pred)
+    ---
+    ### 5. Визуализация результатов.
+    Визуализация результатов подготовлена на основе streamlit и matplotlib
+    st.write(f"Accuracy: {accuracy:.2f}")
+    st.subheader("Confusion Matrix")
+    fig, ax = plt.subplots()
+    sns.heatmap(conf_matrix, annot=True, fmt='d', cmap='Blues', ax=ax)
+    st.pyplot(fig)
+    st.subheader("Classification Report")
+    st.text(classification_rep)
+    ---
     ## Streamlit-приложение
+    Графическая часть состоит из
     - Основная страница: анализ данных и предсказания.
     - Страница с презентацией: описание проекта.
     ---
     ## Заключение
-    - Итоги и возможные улучшения.
+    - Загрузили и предобработали данные 
+    - Обучили регрессионную модель;
+    - Оценили регрессионную модель. Точность составила 0.99, F1-score = 0.99
     """
     # Настройки презентации
     with st.sidebar:
